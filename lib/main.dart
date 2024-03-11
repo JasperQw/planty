239691cd43902bf51f2cd7570d29fa717cd54650
Jasper <ngchingwei253@gmail.com>
2024-03-11 22:54:45 +0800

    setup main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:planty/firebase_options.dart';
+import 'package:planty/utilities/color.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,26 +18,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Start here"),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Planty',
+        theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: background),
+        home: const Placeholder());
   }
 }
--- lib/utilities/color.dart
+++ lib/utilities/color.dart
@@ -2,3 +2,4 @@ import "package:flutter/material.dart";
 
 const black = Color(0xFF000000);
 const green = Color(0xFF4DB470);
+const background = Color(0xFFFFFFFF);
