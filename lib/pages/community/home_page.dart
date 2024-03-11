386c29fea9a353303eeff5e6034ab23dbfa02235
Jasper <ngchingwei253@gmail.com>
2024-03-11 22:50:05 +0800

    added some folders to separate different type of files

---
+++ lib/pages/community/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommunityHome extends StatelessWidget {
+  const CommunityHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
