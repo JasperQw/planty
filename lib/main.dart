4d1ab513f6a7a9e98dbe957604f8d3cfc0da662b
Jasper <ngchingwei253@gmail.com>
2024-03-12 18:28:46 +0800

    Merge with cw/community branch

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const CommunityDetails());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
